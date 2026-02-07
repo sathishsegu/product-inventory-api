38620e21e7fcd1f3ff14691addd26fc28fa420c1
SathishSegu <sathishsegu123@gmail.com>
2026-02-07 14:12:01 +0530

    Day 5 feature init push

---
+++ src/main/java/com/inventory/entity/Category.java
@@ -0,0 +1,20 @@
+package com.inventory.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    private Long categoryId;
+    private String categoryName;
+}
--- src/main/java/com/inventory/service/impl/ProductServiceImpl.java
+++ src/main/java/com/inventory/service/impl/ProductServiceImpl.java
@@ -9,7 +9,6 @@ import com.inventory.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
